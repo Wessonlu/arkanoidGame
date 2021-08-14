1b77985db9795fbf94158fc09bf432f66c488106
luhw <wessonlu@hotmail.com>
2021-08-14 22:33:37 +0800

    修改画布

--- main.js
+++ main.js
@@ -17,15 +17,55 @@
             return Object.prototype.toString.call(type)
         }
     }
+    function draw () {
+
+    }
+    function createCanvas () {
+
+    }
+    
     var ArkanoidGame = function(opt) {
         this.opt = opt
-        var wrapEl = this.getWrapEl()
-        console.log(wrapEl)
     }
+    ArkanoidGame.prototype.start = function() {
+        var wrapEl = this.getWrapEl()
+        this.canvas = this.initCanvas()
+        var ctx = this.getCtx()
+        ctx.fillStyle="#FF0000";
+        ctx.fillRect(0,0,150,75);
+        wrapEl.appendChild(this.canvas);
+    }
+    ArkanoidGame.prototype.initCanvas = function() {
+        var canvas = document.createElement('canvas')
+        var width = '100%'
+        var height = '100%'
+        canvas.style.width = width
+        canvas.style.height = height
+        window.addEventListener('resize', this.resize(canvas))
+        return canvas
+    }
+    ArkanoidGame.prototype.resize = function(canvas) {
+        var wrapEl = this.getWrapEl()
+        canvas.width = wrapEl.offsetWidth
+        canvas.height = wrapEl.offsetHeight
+        return function() {
+            canvas.width = wrapEl.offsetWidth
+            canvas.height = wrapEl.offsetHeight
+        }
+    }
+    ArkanoidGame.prototype.getCtx = function() {
+        return this.canvas.getContext('2d')
+    }
+    // 获取外层容器元素
     ArkanoidGame.prototype.getWrapEl = function() {
         var el = this.opt.el || document.body
-        if(!utils.isString(el)) {
-            el = document.getElementById(el)
+        if(utils.isString(el)) {
+            var reg = /(^\.|#)\w/
+            if(reg.test(el)) {
+                el = document.querySelector(el)
+            } else {
+                el = document.getElementById(el)
+            }
         }
         return el
     }
